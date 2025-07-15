fce17237e6246b8f2cb231d2eda130eddadebad4
PERMADI89 <jangkrik0110@gmail.com>
2025-07-15 11:57:18 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -5,7 +5,7 @@ st.markdown('bismilah')
 import mysql.connector
 import pandas as pd
 connection = mysql.connector.connect(
-  host = "127.0.0.1",
+  host = "localhost",
   port = "3306",
   user = "root",
   password = "",
