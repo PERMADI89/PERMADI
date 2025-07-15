6cb48a44a57cbde25e333cddc789f5373297bbc4
PERMADI89 <jangkrik0110@gmail.com>
2025-07-15 17:34:53 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -2,18 +2,3 @@ import streamlit as st
 
 st.markdown('bismilah')
 
-import mysql.connector
-import pandas as pd
-connection = mysql.connector.connect(
-  host = "localhost",
-  port = "3306",
-  user = "root",
-  password = "",
-  database = "permadi"
-)
-
-cursor = connection.cursor()
-cursor.execute( "select * from members" )
-data = cursor.fetchall()
-df = pd.DataFrame(data,columns=cursor.column_names)
-st.dataframe(df)
