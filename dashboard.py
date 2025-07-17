8e4fc332bd007e90f63e3da48974dc69dc0a9994
PERMADI89 <jangkrik0110@gmail.com>
2025-07-17 16:34:09 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,3 +3,5 @@ import streamlit as st
 def show_dashboard():
     st.title("📊 Dashboard Utama")
     st.markdown("Selamat datang di halaman utama admin.")
+
+    st.markdown("🛠️ Dibuat dengan ❤️ oleh tim Permadi")
