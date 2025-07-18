0e485d9979acff63735865b391071bf7b706e5cd
PERMADI89 <jangkrik0110@gmail.com>
2025-07-18 17:06:47 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,23 @@
 import streamlit as st
 
 def show_dashboard():
-    st.title("📊 Dashboard Admin")
-    st.info("Selamat datang di Dashboard Admin. Silakan pilih menu di sidebar.")
+    st.title("📊 Web Commant Center")
+    st.info("Selamat datang di Dashboard Commant Center. Silakan pilih menu di sidebar.")
+    st.info("Tetap Menyerah jangan Semangat")
+    
+    st.info("Nama - Tim Commant Center")
+    st.info("Andre 'Taulani'")
+    st.info("Herman 'Syah'")
+    st.info("Yusuf 'Kala'")
+    st.info("Wahyu 'Sesat'")
+    st.info("Permadi 'New Daddy'")
+    
+    st.info("Nama - Tim DevOps")
+    st.info("Junedi 'Anying'")
+    st.info("Aji 'Nomoto'")
+
+    st.info("Nama - Tim DB")
+    st.info("Heru 'Roker Dangdut'")
+    st.info("Zaenal 'M'")
+
+
