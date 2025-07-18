21819ada15d22b85beadb89313548c0da172045b
PERMADI89 <jangkrik0110@gmail.com>
2025-07-18 11:24:40 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 def show_dashboard():
-    st.title("📊 Dashboard Utama")
-    st.markdown("Selamat datang di halaman utama admin.")
+    st.title("📊 Dashboard Admin")
+    st.info("Selamat datang di Dashboard Admin. Silakan pilih menu di sidebar.")
