ba6b252c34d27bbfef88a6493f630343e87ffb91
PERMADI89 <jangkrik0110@gmail.com>
2025-07-18 12:56:48 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,58 +1,36 @@
 import sqlite3
-from datetime import datetime
 
-DB_NAME = "data.db"
+DB_NAME = 'data.db'
 
-def init_db():
+def create_login_table():
     conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
     c.execute("""
-        CREATE TABLE IF NOT EXISTS users (
+        CREATE TABLE IF NOT EXISTS users_login (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            tanggal TEXT,
-            name TEXT,
-            email TEXT,
-            umur INTEGER,
-            divisi TEXT,
-            aktivitas TEXT,
-            layanan TEXT,
-            keterangan TEXT,
-            rca TEXT,
-            solusi TEXT,
-            status TEXT
+            username TEXT UNIQUE,
+            password TEXT
         )
     """)
     conn.commit()
     conn.close()
 
-def insert_data(name, email, umur, divisi, aktivitas, layanan, keterangan, rca, solusi, status):
+def add_user_login(username, password):
     conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
-    tanggal = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    c.execute("""
-        INSERT INTO users (
-            tanggal, name, email, umur, divisi, aktivitas, layanan, 
-            keterangan, rca, solusi, status
-        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    """, (tanggal, name, email, umur, divisi, aktivitas, layanan, keterangan, rca, solusi, status))
-    conn.commit()
+    try:
+        c.execute("INSERT INTO users_login (username, password) VALUES (?, ?)", (username, password))
+        conn.commit()
+        result = True
+    except sqlite3.IntegrityError:
+        result = False
     conn.close()
+    return result
 
-def fetch_all():
+def login_user(username, password):
     conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
-    c.execute("""
-        SELECT tanggal, name, email, umur, divisi, aktivitas, layanan, 
-               keterangan, rca, solusi, status 
-        FROM users
-    """)
-    data = c.fetchall()
+    c.execute("SELECT * FROM users_login WHERE username=? AND password=?", (username, password))
+    data = c.fetchone()
     conn.close()
     return data
-
-def update_status(email, status):
-    conn = sqlite3.connect(DB_NAME)
-    c = conn.cursor()
-    c.execute("UPDATE users SET status=? WHERE email=?", (status, email))
-    conn.commit()
-    conn.close()
